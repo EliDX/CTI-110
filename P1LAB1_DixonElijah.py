8a8a3ff7adc2a32adceb1929e01957af1679bfd5
EliDX <dixeli5567@gmail.com>
2024-09-24 14:21:15 -0400

    Add files via upload

---
+++ P1LAB1_DixonElijah.py
@@ -0,0 +1,13 @@
+# Elijah Dixon
+# 9/24/2024
+# P1LAB1
+# get input from the user and display an output
+
+#Get last name from user.
+FirstName = input("Enter your first name: ")
+
+#Get the last name from user.
+LastName = input("Enter your last name: ")
+
+#Display info to the user
+print("Hello,", FirstName, LastName + "! Welcome to CTI-110")
