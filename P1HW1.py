a493f3e492a703f44df3e7b6be4f6b12b2941f8f
EliDX <dixeli5567@gmail.com>
2024-09-24 14:36:26 -0400

    Add files via upload

---
+++ P1HW1.py
@@ -0,0 +1,15 @@
+# Elijah Dixon
+# 9/26/2024
+# P1HW1
+# Get integer input from from the user and perform calcs (thats slang for "calculations").
+
+# Display output to user
+print("-----Calculating Exponents-----")
+print
+print
+print
+("Enter an integer as the base value: ")
+("Enter an integer as the exponent: ")
+print
+print
+print
