a9cbf7cf0165fdf1f38319620526ce932314206d
EliDX <dixeli5567@gmail.com>
2024-11-07 14:49:05 -0500

    Add files via upload

---
+++ P4LAB2_DixonElijah.py
@@ -0,0 +1,14 @@
+# ELijah Dixon
+# 11/7/24
+# P4LAB2
+# Using loops to calculate only positive times tables.
+
+runagain = "yes"
+while runagain != "no":
+# Get an integer from the user.
+# If integer is 0 or higher, show times table.
+# Use a loop to show the times table.
+    print("Show times table if user enters a positive integer.")
+    run_again = input("Wanna see me do it again?")
+
+print("Goodbye!")
