56cea65eaf1b2ba9f2cf193466f4bacf4a72d9c3
EliDX <dixeli5567@gmail.com>
2024-11-19 13:49:15 -0500

    Update P4HW1_DixonElijah.py
    Finished, but turned in late...

--- P4HW1_DixonElijah.py
+++ P4HW1_DixonElijah.py
@@ -26,35 +26,30 @@ while numscoresQ == False:
 
 numscores = int(numscores)
 
-'''
-# This will serve to work the same as the numscoresQ code did, bit for scoreinput.
-scoreinputQ = False
-while scoreinputQ == False:
+# Loop to get each item neccessary. I used a try exept mixed with a while loop to prevent letters and numbers over 100 or under 0.
+
+for item in range(numscores):
+    scoreinputQ = False
+    while scoreinputQ == False:
         try:
-            int(numscores)
-        except:
-            print("Only numbers, please.")
-            scoreinputQ = False
-            scoresinput = input("How many socres would you like to enter? ")
+            scoreinput = float(input(f"Enter score #{item + 1}: "))
+            print()
+            while scoreinput > 100 or scoreinput < 0:
+                print(f"Sorry, {scoreinput} is not a valid score. Only numbers between 0-100 allowed.")
+                scoreinput = float(input(f"Enter score #{item + 1} again: "))
+                print()
+        except ValueError:
+            print("No letters allowed, please!")
             print()
         else:
             scoreinputQ = True
-'''
-
-# Loop to get each item neccessary. 
-for item in range(numscores):
-    scoreinput = int(input(f"Enter score #{item + 1}: "))
-
-    # Loop to make sure this score is VALID.
-    while scoreinput > 100 or scoreinput < 0:
-        print(f"Sorry, {scoreinput} is not a valid score. Only numbers between 0-100 allowed.")
-        scoreinput = int(input(f"Enter score #{item + 1} again, please: "))
-    # Adding this score to the list.
+            
     scores.append(scoreinput)
 
 # Printing and formatting time.
-# I will set variables for the lowest score, average, and reuse the code I made in P2HW2 to get the letter grade. I added the sum to get the average, and the highest grade just because.
-# I concidered chaning the variable names to use "scr" to abbreviate score since I used that so much, but it wouldve messed with the big if else staement to  find the letter score.
+# I will set variables for the lowest score, average, and reuse the code I made in P2HW2 to get the letter grade.
+# I added the sum to get the average, and the highest grade just because.
+# I concidered chaning the variable names to use "scr" to abbreviate score since I used that so much, but it wouldve messed with the if/else.
 
 LoGrd = min(scores)
 HiGrd = max(scores)
@@ -64,33 +59,3 @@ AvgGrd = SumGrd/len(scores)
 # This is the grade-getting function.
 if AvgGrd >= 90:
     LGrade = "A"
-
-if AvgGrd >= 80 and AvgGrd <= 89:
-    LGrade = "B"
-
-if AvgGrd >= 70 and AvgGrd <= 79:
-    LGrade = "C"
-
-if AvgGrd >= 60 and AvgGrd <= 69:
-    LGrade = "D"
-
-if AvgGrd >= 0 and AvgGrd <= 59:
-    LGrade = "F"
-
-# The results. (edited version of the older one.)
-print()
-print("------------Results!-------------")
-print()
-print(f"{'Lowest Grade: ':<20}{LoGrd:.1f}")
-scores.remove(LoGrd)
-print(f"{'Modified List: ':<20}{scores}")
-print(f"{'Average: ':<20}{AvgGrd:.2f}")
-print(f"{'Sores Average: ':<20}{LGrade}") 
-print()
-print("-----------------------------------------")
-
-
-
-# To prevent the program from instantly closing.
-print()
-input("Press ENTER to close.")
