4fbdeb487cae6a08a0fbe1b45e7b3a022841863a
EliDX <dixeli5567@gmail.com>
2024-12-03 14:40:31 -0500

    Update gaming.py

--- gaming.py
+++ gaming.py
@@ -2,12 +2,17 @@
 # 11/2024
 # Stupid dumb dumb stupid game
 
+import random
+def player_party():
+
+
+    
 def create_character():
     name = input("Enter characater name: ")
     health = float(input(f"Enter {name}'s health: "))
     inventory = []
     weapons = ["bazooka"]
-    strength = int(input(f"Enter [name]'s strength: "))
+    strength = int(input(f"Enter {name}'s strength: "))
 
     character = {
         "Name": name,
@@ -18,19 +23,49 @@ def create_character():
         }
     return character
 
+def create_villain():
+    name = "Dastardly Horseradish"
+    health = float(8)
+    inventory = ["horseradish", "horse", "radish"]
+    weapons = ["mason jar"]
+    strength = int(2)
+
+    character = {
+        "Name": name,
+        "Health": health,
+        "Items": inventory,
+        "Weapons": weapons,
+        "Strength": strength,
+        }
+    return character
+
+def battle_test(char1, horseradish):
+    while char1['Health'] > 0 and horseradish['Health'] > 0:
+        # Define characters...
+        characters = [char1, horseradish]
+
+        # Randomly shuffle the characters
+        random.shuffle(characters)
+
+        # Assign attacker and victim
+        attacker = characters[0]
+        defender = characters[1]
+
+        # Damage calculator...
+        
+        
+        print(f"{characters} are duking it out?!")
+        print(f"{attacker} attacks!")
+        print(f"{defender} takes {damage} damage...")
+        
 
 
-
-
-def main():
-    print("awaken, my child")
+def show_stats(character):
+    # Character is a dictionary
     print()
-    # Call create character
-    char1 = create_character()
+    print(f"{character['Name']}'s Stats...")
+    print(f"Health: {character['Health']}")
+    print(f"Strength: {character['Strength']}")
+    print(f"Weapons: {character['Weapons']}")
+    print(f"Items: {character['Items']}")
     print()
-    print(char1)
-
-
-# Calling your moms house.
-if __name__ == "__main__":
-    main()
